51750b37cb3b429db750b9301dbdb7c6511374f0
johame72 <134301243+johame72@users.noreply.github.com>
2023-09-04 13:20:14 -0500

    Update server.js
    Matching the local laptop files

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const PORT = 8105;
 
 app.use(express.static('public')); // Serve static files from the 'public' directory
 
+app.get('/lastfm-callback', (req, res) => {
+    const token = req.query.token;
+    if (!token) {
+        return res.status(400).send('Token is missing!');
+    }
+    // Handle the token, perhaps save it, use it to fetch an access token, etc.
+    res.send('Last.fm authentication successful!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
